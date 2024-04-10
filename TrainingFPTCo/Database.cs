5000797b3a6da3ba8ebb1e7bf1f855adb6d2d4f6
khanh <129238612+khanh052003@users.noreply.github.com>
2024-04-10 19:29:16 +0700

    update
    update

---
+++ TrainingFPTCo/Database.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace TrainingFPTCo
+{
+    public class Database
+    {
+        public static string GetStrConnection()
+        {
+            string strConnection = @"Data Source=21AK22-COM\KHANHDNBH0021;Initial Catalog=Training;Integrated Security=True;Encrypt=True";
+            return strConnection;
+        }
+
+        public static SqlConnection GetSqlConnection() 
+        {
+            string strConnection = Database.GetStrConnection();
+            SqlConnection conn = new SqlConnection(strConnection);
+            return conn;
+        }
+    }
+}
